f3f3a178332587e4199835de578af3242f85e76f
ajay2589 <ajay2589@gmail.com>
2015-01-07 16:08:13 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -17,7 +17,7 @@ dataWithinPeriod$DateTime <- as.POSIXct(dateTime)
 ##Making Plots
 
 #Histogram of Global_active_power
-hist(dataWithinTime$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+hist(dataWithinPeriod$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 #Save as PNG file
 dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
