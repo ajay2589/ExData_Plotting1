86ae9c063aa33301b4347229ba9df4486fc103ba
ajay2589 <ajay2589@gmail.com>
2015-01-07 15:34:29 +0530

    Remove plot1

--- plot2.R
+++ plot2.R
@@ -17,7 +17,6 @@ dataWithinPeriod$DateTime <- as.POSIXct(dateTime)
 ##Making Plots
 
 #Plot of Global_active_power
-#hist(dataWithinTime$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 plot(dataWithinPeriod$Global_active_power~dataWithinPeriod$DateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
 #Save as PNG file
 dev.copy(png, file="plot2.png", height=480, width=480)
