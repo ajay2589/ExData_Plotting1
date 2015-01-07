cd13ebd6847043956058a4f0b9a52769cbc3fd98
ajay2589 <ajay2589@gmail.com>
2015-01-07 15:39:41 +0530

    submetering

--- plot3.R
+++ plot3.R
@@ -13,3 +13,12 @@ dataWithinPeriod <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02
 #Convert the Date and Time variables using as.Date() and as.POSIXct()
 dateTime <- paste(as.Date(dataWithinPeriod$Date), dataWithinPeriod$Time)
 dataWithinPeriod$DateTime <- as.POSIXct(dateTime)
+
+##Making Plots
+
+#Plot of Sub_metering
+with(dataWithinPeriod, {plot(Sub_metering_1~DateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="");lines(Sub_metering_2~DateTime,col='Red');lines(Sub_metering_3~DateTime,col='Blue')})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+#Save as PNG file
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
