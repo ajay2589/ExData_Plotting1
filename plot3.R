148aa7b6e329d682c69e40632b3a2c2c2b35a9c1
ajay2589 <ajay2589@gmail.com>
2015-01-07 22:12:50 +0530

    changes to 3 and 4

--- plot3.R
+++ plot3.R
@@ -17,7 +17,7 @@ dataWithinPeriod$DateTime <- as.POSIXct(dateTime)
 ##Making Plots
 
 #Plot of Sub_metering
-with(dataWithinPeriod, {plot(Sub_metering_1~DateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="");lines(Sub_metering_2~DateTime,col='Red');lines(Sub_metering_3~DateTime,col='Blue')})
+with(dataWithinPeriod, {plot(Sub_metering_1~DateTime, type="l", ylab="Energy sub metering", xlab="");lines(Sub_metering_2~DateTime,col='Red');lines(Sub_metering_3~DateTime,col='Blue')})
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 #Save as PNG file
 dev.copy(png, file="plot3.png", height=480, width=480)
