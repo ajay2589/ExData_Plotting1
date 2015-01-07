6f1f4c1748246a3b406f45acf1bdcce9b62661be
ajay2589 <ajay2589@gmail.com>
2015-01-07 15:44:02 +0530

    combine plots

--- plot4.R
+++ plot4.R
@@ -13,3 +13,22 @@ dataWithinPeriod <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02
 #Convert the Date and Time variables using as.Date() and as.POSIXct()
 dateTime <- paste(as.Date(dataWithinPeriod$Date), dataWithinPeriod$Time)
 dataWithinPeriod$DateTime <- as.POSIXct(dateTime)
+
+
+
+##Making Plots
+
+#Plot of Sub_metering
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(dataWithinPeriod, {
+  plot(Global_active_power~DateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+  plot(Voltage~DateTime, type="l", ylab="Voltage (volt)", xlab="")
+  plot(Sub_metering_1~DateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~DateTime,col='Red')
+  lines(Sub_metering_3~DateTime,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power~DateTime, type="l", ylab="Global Rective Power (kilowatts)",xlab="")
+})
+#Save as PNG file
+dev.copy(png, file="plot4.png", height=480, width=480)
+dev.off()
